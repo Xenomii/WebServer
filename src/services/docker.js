ae37c32120554d970fc6838e40fa728ddcfc34e2
Xenomii <voltzxp@gmail.com>
2022-07-05 09:14:34 +0800

    Added mechanism to detect whether the web app can connect to the docker container

--- src/services/docker.js
+++ src/services/docker.js
@@ -6,11 +6,13 @@ var docker = new Docker();
 var container_name = 'recursing_volhard';
 // Acquires the docker container
 var container = docker.getContainer(container_name);
-// Checks whether the web application can connect to the docker container
-if (!container) {
-    console.log(`Error! Could not connect to container ${container_name}...`);
-} else {
-    console.log("Docker container connection is OK");
-}
 
+// Checks whether the web application can connect to the docker container
+container.inspect(container_name, function (err, data) {
+    if (err) {
+        console.log('ERROR: DOCKER IS NOT CONNECTED!');
+    } else {
+        console.log('Docker Container Connection OK');
+    }
+})
 exports.container = container;
